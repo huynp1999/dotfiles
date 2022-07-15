7f5f079901d2ac541b2b43cd3cccdb44ce85dc67
huynp1999 <huynp1999@gmail.com>
2022-07-15 15:01:16 +0700

    [add]

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
+packages=`curl https://raw.githubusercontent.com/huynp1999/dotfiles/master/packages`
 if [ -n "$(which yum)" ]
 then
-	yum install `cat ./packages` -y
+	yum install $packages -y
 else
-	apt install `cat ./packages` -y
+	apt install $packages -y
 fi
 
 # setup vim
@@ -11,8 +12,23 @@ ln -s /usr/bin/vim /usr/bin/vi
 cp -r ./.vim* ~
 
 #setup copy bashrc
+iphostname=$(hostname -I)+$(hostname)
+if grep -Fxq "$iphostname" /etc/hosts
+then
+	echo `hostname -I` `hostname` >> /etc/hosts
+fi
+
 cat << EOF >> ~/.bashrc
+# enable bash completion
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+# copy previous commands: 1. Ctrl+] 2. xc
 bind '"\C-]":"\C-e\C-u xclip -selection clipboard <<"EOF"\n\C-y\nEOF\n"'
+alias xc='history 2 | cut -c 8- | head -n 1 | xclip -selection clipboard'
 EOF
-echo `hostname -I` `hostname` >> /etc/hosts
-echo "alias xc='history 2 | cut -c 8- | head -n 1 | xclip -selection clipboard'" >> ~/.bashrc
+bash
